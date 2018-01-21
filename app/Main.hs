4595d40f90a1c4f5de033ac7a66435913e6ee1ac
Iruyan_Zak <yuri.kazuna@gmail.com>
2018-01-22 00:43:44 +0900

    ルーティングを書いてみた

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import TcpServer
 import Service
+import qualified Data.ByteString.Char8 as BS
 
 main :: IO ()
-main = runServer "127.0.0.1" "5000" echoOK
+main = runServer "127.0.0.1" "5000" $ serve route
+
+
+route :: Request -> Responce
+route (Request _ []) = (Responce 200 "root dir")
+route (Request GET ["user", userName]) = (Responce 200 (BS.append "You are " userName))
+route (Request _ segments) = (Responce 200 (BS.append "You accessed " $ BS.intercalate "/" segments))
+
